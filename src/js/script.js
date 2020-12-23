1be8d76cc567c37f547ac3d4896624f4cb510c87
narzillo83 <narzillomirzabaev@gmail.com>
2020-12-23 12:08:18 +0500

    2020-12-23-01

--- src/js/script.js
+++ src/js/script.js
@@ -5,4 +5,4 @@ $(document).ready(function(){
       prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left.svg"></button>',
       nextArrow: '<button type="button" class="slick-next"><img src="../icons/right.svg"></button>'
     });
-  }); 
+  });

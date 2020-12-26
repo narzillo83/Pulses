d23233a4b6b26fcf889aeb630c7e26f991fb0e16
narzillo83 <narzillomirzabaev@gmail.com>
2020-12-26 10:25:46 +0500

    2020-12-26-01

--- src/js/script.js
+++ src/js/script.js
@@ -66,4 +66,6 @@ $(document).ready(function(){
 	valideForms('#consultation-form');
 	valideForms('#consultation form');
 	valideForms('#order form');
+
+	$('input[name=phone]').mask('+999 (99) 999-99-99');
 });	

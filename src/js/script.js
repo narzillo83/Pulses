1169a959ec19facefb00c6ffdf2fcaff30842d5e
narzillo83 <narzillomirzabaev@gmail.com>
2020-12-27 10:52:35 +0500

    2020-12-27-01

--- src/js/script.js
+++ src/js/script.js
@@ -68,4 +68,31 @@ $(document).ready(function(){
 	valideForms('#order form');
 
 	$('input[name=phone]').mask('+999 (99) 999-99-99');
-});	
+
+	$('form').submit(function(e) {
+		e.preventDefault();
+		$.ajax({
+			type: "POST",
+			url: "mailer/smart.php",
+			data: $(this).serialize()
+		}).done(function() {
+			$(this).find("input").val("");
+
+
+			$('form').trigger('reset');
+		});
+		return false;
+	});
+	$(window).scroll(function() {
+		if ($(this).scrollTop() > 1600) {
+			$('.pageup').fadeIn();
+		} else {
+			$('.pageup').fadeOut();
+		}
+	});
+	$("a[href^='#']").click(function(){
+		const _href = $(this).attr("href");
+		$("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+		return false;
+	});
+});	 
